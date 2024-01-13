f2fe284bdc0d6f9a3964602b56b90332e5c6941b
Daniel Kral <dakral@protonmail.com>
2024-01-13 13:46:09 +0100

    Remove redundant category sql scripts

--- src/main/resources/migrations/v1/create/create_article_category.sql
+++
@@ -1,8 +0,0 @@
-create table Article_Category
-(
-    article_id  integer not null,
-    category_id integer not null,
-    constraint pk_Article_Category primary key (article_id, category_id),
-    constraint fk_Article_Category_Article foreign key (article_id) references Article (id),
-    constraint fk_Article_Category_Category foreign key (category_id) references Category (id)
-)
--- src/main/resources/migrations/v1/create/create_store_category.sql
+++
@@ -1,9 +0,0 @@
-create table Store_Category
-(
-    store_id          integer     not null,
-    store_category_id varchar(50) not null,
-    category_id       integer     not null,
-    constraint pk_Store_Category primary key (store_id, store_category_id),
-    constraint fk_Store_Category_Store foreign key (store_id) references Store (id),
-    constraint fk_Store_Category_Category foreign key (category_id) references Category (id)
-)
