bb146ea485dcc083d380234e090ebe019d45afe3
Daniel Kral <dakral@protonmail.com>
2024-01-13 12:49:43 +0100

    Fix wrong foreign key table name in create_article.sql; Rename unit_id to article_unit_id

--- src/main/resources/migrations/v1/create/create_article.sql
+++ src/main/resources/migrations/v1/create/create_article.sql
@@ -5,12 +5,12 @@ create table Article
     name              varchar(100)   not null,
     description       clob,
     origin_country_id integer,
-    unit_id           integer        not null,
+    article_unit_id   integer        not null,
     quantity          numeric(18, 4) not null,
     weightable        number(1)      not null,
     constraint pk_Article primary key (id),
-    constraint uc_Article_Name_Brand unique (brand_id, name, unit_id, quantity),
+    constraint uc_Article_Name_Brand unique (brand_id, name, article_unit_id, quantity),
     constraint fk_Article_Brand foreign key (brand_id) references Brand (company_id),
     constraint fk_Article_Country_origin foreign key (origin_country_id) references Country (id),
-    constraint fk_Article_Unit foreign key (unit_id) references Measurement_Unit (id)
+    constraint fk_Article_Unit foreign key (article_unit_id) references Article_Unit (id)
 )
