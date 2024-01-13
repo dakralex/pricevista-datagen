941271f8a6542f1b2e172150372c131899d88693
Daniel Kral <dakral@protonmail.com>
2024-01-13 13:40:39 +0100

    Rename BillaPromotionType to BillaJsonPromotionType

--- src/main/kotlin/org/dakralex/pricevista/parser/billa/BillaJsonPriceCategory.kt
+++ src/main/kotlin/org/dakralex/pricevista/parser/billa/BillaJsonPriceCategory.kt
@@ -7,7 +7,7 @@ data class BillaJsonPriceCategory(
     val perStandardizedQuantity: BillaJsonPrice,
     val promotionQuantity: Int? = null,
     val promotionText: String? = null,
-    val promotionType: BillaPromotionType? = null,
+    val promotionType: BillaJsonPromotionType? = null,
     val promotionValue: BillaJsonPrice? = null,
     val promotionValuePerStandardizedQuantity: Int? = null,
     val tags: List<String>, // TODO Use enum class JsonBillaPriceTag in the future
--- src/main/kotlin/org/dakralex/pricevista/parser/billa/BillaJsonPromotionType.kt
+++ src/main/kotlin/org/dakralex/pricevista/parser/billa/BillaJsonPromotionType.kt
@@ -3,7 +3,7 @@ package org.dakralex.pricevista.parser.billa
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class BillaPromotionType {
+enum class BillaJsonPromotionType {
     FROM,
     PER_SET_OF
 }
