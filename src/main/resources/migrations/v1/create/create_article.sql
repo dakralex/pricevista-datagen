07683fc101a20cb5bff4cba8ec2e5ac8afb48498
Daniel Kral <dakral@protonmail.com>
2024-01-24 00:17:02 +0100

    Reformat create_article.sql

--- src/main/resources/migrations/v1/create/create_article.sql
+++ src/main/resources/migrations/v1/create/create_article.sql
@@ -9,8 +9,8 @@ create table Article
     quantity          numeric(18, 4) not null,
     weightable        number(1)      not null,
     constraint pk_Article primary key (id),
-    constraint uc_Article unique (brand_id, name, article_unit_id, quantity,
-                                  weightable),
+    constraint uc_Article unique (brand_id, name,
+                                  article_unit_id, quantity, weightable),
     constraint fk_Article_Brand foreign key (brand_id) references Brand (company_id),
     constraint fk_Article_Country_origin foreign key (origin_country_id) references Country (id),
     constraint fk_Article_Unit foreign key (article_unit_id) references Article_Unit (id)
