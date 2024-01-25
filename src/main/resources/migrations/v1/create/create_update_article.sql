3b9bfcf253586d0ca004504187517c7e63f3b5ca
Daniel Kral <dakral@protonmail.com>
2024-01-25 10:50:17 +0100

    Add delete on redundant brands when a Article is updated with update_article

--- src/main/resources/migrations/v1/create/create_update_article.sql
+++ src/main/resources/migrations/v1/create/create_update_article.sql
@@ -1,10 +1,13 @@
 create or replace procedure update_article(
     p_article_id in integer,
-    p_article_name in varchar2 default null,
-    p_article_brand_name in varchar2 default null,
-    p_article_description in clob default null
+    p_article_name in varchar default null,
+    p_article_brand_name in varchar default null,
+    p_article_description in clob default null,
+    p_update_count out integer
 ) as
-    lv_brand_id integer := null;
+    lv_brand_id         integer := null;
+    lv_current_brand_id integer := null;
+    lv_brand_usages     integer := 0;
 begin
     if p_article_id is null then
         raise_application_error(-20000, 'The article id must not be null.');
@@ -39,8 +42,37 @@ begin
         description = nvl(p_article_description, description)
     where id = p_article_id;
 
-    -- If nothing was updated, notify the user of the procedure
-    if sql%rowcount = 0 then
-        raise_application_error(-20000, 'Nothing was updated.');
+    if p_article_brand_name is not null then
+        -- Find out the current brand id of the article
+        begin
+            select a.brand_id
+            into lv_current_brand_id
+            from Article a
+            where a.id = p_article_id;
+        exception
+            when no_data_found then lv_current_brand_id := null;
+        end;
+
+        -- If the brand is not used by any article, delete the entry
+        if lv_current_brand_id is not null and
+           lv_current_brand_id <> lv_brand_id then
+            select count(*)
+            into lv_brand_usages
+            from Article
+            where brand_id = lv_current_brand_id;
+
+            -- If the brand is not used by any other article, delete the entry
+            if lv_brand_usages = 0 then
+                delete Brand where company_id = lv_current_brand_id;
+                delete Company where id = lv_current_brand_id;
+            end if;
+        end if;
     end if;
+
+    -- Will always be at least 1, but it is only to have an output
+    -- because of the course requirements
+    p_update_count := sql%rowcount;
 end;
+
+
+
